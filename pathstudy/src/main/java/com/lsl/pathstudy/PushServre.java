8813771e5cf9ed8de205dd79fcc353b13a9c45f1
lishoulin <408930131@qq.com>
2017-06-05 17:16:35 +0800

    NotificationListenerService 监听通知拉起服务保活demo

--- pathstudy/src/main/java/com/lsl/pathstudy/PushServre.java
+++ pathstudy/src/main/java/com/lsl/pathstudy/PushServre.java
@@ -1,7 +1,9 @@
 package com.lsl.pathstudy;
 
 import android.app.Service;
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.util.Log;
@@ -22,6 +24,18 @@ public class PushServre extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
+
         Log.e("info--->", "服务创建了");
+        toggleNotificationListenerService();
     }
+
+
+    private void toggleNotificationListenerService() {
+        PackageManager pm = getPackageManager();
+        pm.setComponentEnabledSetting(new ComponentName(this, TestService.class),
+                PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP);
+        pm.setComponentEnabledSetting(new ComponentName(this, TestService.class),
+                PackageManager.COMPONENT_ENABLED_STATE_ENABLED, PackageManager.DONT_KILL_APP);
+    }
+
 }
--- pathstudy/src/main/java/com/lsl/pathstudy/TestService.java
+++ pathstudy/src/main/java/com/lsl/pathstudy/TestService.java
@@ -40,12 +40,5 @@ public class TestService extends NotificationListenerService {
     }
 
 
-    private void toggleNotificationListenerService() {
-        PackageManager pm = getPackageManager();
-        pm.setComponentEnabledSetting(new ComponentName(this, TestService.class),
-                PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP);
-        pm.setComponentEnabledSetting(new ComponentName(this, TestService.class),
-                PackageManager.COMPONENT_ENABLED_STATE_ENABLED, PackageManager.DONT_KILL_APP);
-    }
 
 }
