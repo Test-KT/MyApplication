e2c1dd872c652720e99f7e0127e2e56e45897a80
lishoulin <408930131@qq.com>
2017-06-26 17:52:18 +0800

    RxJava2 第三章操作符学习 学习博客链接：http://www.jianshu.com/p/8818b98c44e2
    学习map，flatmap操作符

---
+++ rx2/src/main/java/com/hontek/rx2/Main3Activity.java
@@ -0,0 +1,64 @@
+package com.hontek.rx2;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Function;
+
+public class Main3Activity extends AppCompatActivity {
+
+    String tag = "info--->";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+
+        Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(ObservableEmitter<Integer> e) throws Exception {
+                e.onNext(1);
+                e.onNext(2);
+                e.onNext(3);
+                e.onNext(4);
+            }
+        }).map(new Function<Integer, String>() {  //map操作符可以对数据进行转换
+
+            @Override
+            public String apply(Integer integer) throws Exception {
+                return "this is " + integer;
+            }
+        }).subscribe(new Observer<String>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+
+            }
+
+            @Override
+            public void onNext(String value) {
+                Log.e(tag, value);
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        });
+
+
+
+
+    }
+
+}
