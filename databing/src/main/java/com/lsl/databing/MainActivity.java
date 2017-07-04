20f460566cfe5b280e9405c4fdabe6eb1df0c5a7
lishoulin <408930131@qq.com>
2017-07-04 16:35:27 +0800

    databinding study

---
+++ databing/src/main/java/com/lsl/databing/MainActivity.java
@@ -0,0 +1,26 @@
+package com.lsl.databing;
+
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.lsl.databing.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityMainBinding binding= DataBindingUtil.setContentView(this,R.layout.activity_main);
+        binding.setUser(new User(1,"lsl"));
+        binding.setHandlesr(new handler());
+
+    }
+
+
+
+
+}
---
+++ databing/src/main/java/com/lsl/databing/User.java
@@ -0,0 +1,17 @@
+package com.lsl.databing;
+
+/**
+ * Description:
+ * Author   :lishoulin
+ * Date     :2017/7/4.
+ */
+
+public class User {
+    public final int id;
+    public final String name;
+
+    public User(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
