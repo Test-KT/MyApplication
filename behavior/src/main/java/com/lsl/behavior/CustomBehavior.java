3b28244b443db8444f617c27e47b3cd70c1cdeaa
lsl <408930131@qq.com>
2017-10-24 16:27:59 +0800

    easy behavior学习使用

---
+++ behavior/src/main/java/com/lsl/behavior/CustomBehavior.java
@@ -0,0 +1,33 @@
+package com.lsl.behavior;
+
+import android.content.Context;
+import android.support.design.widget.CoordinatorLayout;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+/**
+ * Created by Administrator on 2017/10/24.
+ */
+
+public class CustomBehavior extends CoordinatorLayout.Behavior<TextView> {
+
+
+    public CustomBehavior(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean layoutDependsOn(CoordinatorLayout parent, TextView child, View dependency) {
+        return dependency instanceof Button;
+    }
+
+    @Override
+    public boolean onDependentViewChanged(CoordinatorLayout parent, TextView child, View dependency) {
+        child.setX(dependency.getX() + 200);
+        child.setY(dependency.getY() + 200);
+        child.setText(dependency.getX() + "," + dependency.getY());
+        return true;
+    }
+}
--- behavior/src/main/java/com/lsl/behavior/MainActivity.java
+++ behavior/src/main/java/com/lsl/behavior/MainActivity.java
@@ -2,6 +2,8 @@ package com.lsl.behavior;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.btn).setOnTouchListener(new View.OnTouchListener() {
+            @Override public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()){
+
+                    case MotionEvent.ACTION_MOVE:
+                        v.setX(event.getRawX()-v.getWidth()/2);
+                        v.setY(event.getRawY()-v.getHeight()/2);
+                        break;
+                }
+                return false;
+            }
+        });
     }
 }
